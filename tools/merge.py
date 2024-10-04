bbf162b71542247f4d75bb527f4b0d7c718d05f0
Mohamed Yosef <mohamedyosef101@outlook.com>
2024-10-04 22:09:15 +0100

    maybe I'll change this tool name

--- tools/merge.py
+++ tools/merge.py
@@ -3,7 +3,6 @@ import pandas as pd
 # load the datasets
 PATH = 'E:/projects/active/olympics-economics/data/'
 medals_total = pd.read_csv(PATH + 'kaggle-olympics2024.csv')
-country_code = pd.read_csv(PATH + 'iban-country_codes.csv')
 gdp = pd.read_csv(PATH + 'world_bank-gdp_per_capita-2023.csv')
 
 
@@ -11,5 +10,5 @@ gdp = pd.read_csv(PATH + 'world_bank-gdp_per_capita-2023.csv')
 df = pd.merge(medals_total, gdp, on='country_code', how='left')
 
 # check
-countries = df.country_code[df['gdp2020'].isnull()]
+countries = df.country_code[df['gdp2023'].isnull()]
 print(countries)
